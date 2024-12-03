833dd6d0ae7d826b6f3c22eaedb5a1f9074b2af2
neeeeding <abc01096780866@gmail.com>
2024-12-04 01:42:17 +0900

    Fix CountTime

--- Assets/01.Script/JYE/CountTime.cs
+++ Assets/01.Script/JYE/CountTime.cs
@@ -8,13 +8,16 @@ using TMPro;
 
 public class CountTime : MonoBehaviour
 {
-    private Timer timer;
+    [SerializeField] private Timer timer;
+
+    [SerializeField] bool perfect;
 
     private TextMeshProUGUI timeText;
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private TextMeshProUGUI DangoText;
 
     private void Awake()
     {
-        timer = GetComponent<Timer>();
         timeText = GetComponent<TextMeshProUGUI>();
         timer.OnTimerEnd += NextScene;
     }
@@ -26,11 +29,17 @@ public class CountTime : MonoBehaviour
 
     private void NextScene()
     {
+        perfect = CheckText();
         //다음씬으로
         SceneManager.LoadScene("Stage5");
     }
 
-    private void TimeText()
+    private bool CheckText() //완벽하게 점수 맞는지
+    {
+        return int.Parse(text.text) == int.Parse(DangoText.text);
+    }
+
+    private void TimeText() //시간 텍스트
     {
         timeText.text = ((int)timer.Timetime).ToString();
     }
