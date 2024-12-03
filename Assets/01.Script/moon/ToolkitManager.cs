65d7171ef30d41dff4453d98c3f953c6ed3db49d
gdhsf3701 <gdhsf3701@gmail.com>
2024-12-03 23:58:36 +0900

    Feat : UImanager

---
+++ Assets/01.Script/moon/ToolkitManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ToolkitManager : MonoBehaviour
+{
+   static public ToolkitManager Instance { get; private set; }
+    private Label ChangName;
+    private Label scroe;
+
+    private void Awake()
+    {
+        if(Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+    private void Start()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        ChangName = root.Q<Label>("ScenName");
+        scroe = root.Q<Label>("Score");
+
+
+    }
+
+    public void ChangeSceneName(string name)
+    {
+        ChangName.text = "당고 만들기 게임.exe : " + name;
+    }
+
+    public void SetScore(int Score)
+    {
+       scroe.text = "현재점수 : " + Score.ToString();
+    }
+
+
+}
