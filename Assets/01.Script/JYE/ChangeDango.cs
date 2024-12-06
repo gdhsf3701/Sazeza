f2a6143674f4b8b1631db7472f1688b98a7d2c8c
neeeeding <abc01096780866@gmail.com>
2024-12-06 10:31:34 +0900

    Fix : ChangeDango

--- Assets/01.Script/JYE/ChangeDango.cs
+++ Assets/01.Script/JYE/ChangeDango.cs
@@ -7,6 +7,9 @@ public class ChangeDango : MonoBehaviour
     [SerializeField] private SpriteRenderer _sprite1;
     [SerializeField] private SpriteRenderer _sprite2;
     [SerializeField] private SpriteRenderer _sprite3;
+    [SerializeField] private SpriteMask _sprite11;
+    [SerializeField] private SpriteMask _sprite22;
+    [SerializeField] private SpriteMask _sprite33;
 
     private void Awake()
     {
@@ -17,5 +20,8 @@ public class ChangeDango : MonoBehaviour
         _sprite1.sprite = GameManager.Instance.IngredientSO[GameManager.Instance.Ingredient[0]].sprite;
         _sprite2.sprite = GameManager.Instance.IngredientSO[GameManager.Instance.Ingredient[1]].sprite;
         _sprite3.sprite = GameManager.Instance.IngredientSO[GameManager.Instance.Ingredient[2]].sprite;
+        _sprite11.sprite = GameManager.Instance.IngredientSO[GameManager.Instance.Ingredient[0]].sprite;
+        _sprite22.sprite = GameManager.Instance.IngredientSO[GameManager.Instance.Ingredient[1]].sprite;
+        _sprite33.sprite = GameManager.Instance.IngredientSO[GameManager.Instance.Ingredient[2]].sprite;
     }
 }
