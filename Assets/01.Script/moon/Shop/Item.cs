e242c2c115b612d14e7b00f450660d111a3ec85b
moon080411 <anstjdalsdlek@gmail.com>
2024-12-12 20:07:14 +0900

    feat:ShopScroll

--- Assets/01.Script/moon/Shop/Item.cs
+++ Assets/01.Script/moon/Shop/Item.cs
@@ -2,18 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Item : MonoBehaviour
 {
     public IngredientTypeEnum MyIngredientType;
     private TextMeshPro text;
-    private SpriteRenderer myRenderer;
+    private Image myRenderer;
+    private Button button;
     public void Initialize()
     {
-        myRenderer = GetComponent<SpriteRenderer>();
+        myRenderer = GetComponentInChildren<Image>();
         text = GetComponentInChildren<TextMeshPro>();
+        button = GetComponent<Button>();
         text.text = $"Coin:{GameManager.Instance.IngredientSO[MyIngredientType].cost}";
         myRenderer.sprite = GameManager.Instance.IngredientSO[MyIngredientType].sprite;
+        button.onClick.AddListener(Buy);
     }
     public void Buy()
     {
--- Assets/01.Script/moon/Shop/ShopSetting.cs
+++ Assets/01.Script/moon/Shop/ShopSetting.cs
@@ -7,24 +7,25 @@ using Random = UnityEngine.Random;
 
 public class ShopSetting : MonoBehaviour
 {
-    [SerializeField] private int xCardMany;
-    [SerializeField] private int yCardMany;
-    [SerializeField] private float distance = 0.1f;
+    //[SerializeField] private int xCardMany;
+    //[SerializeField] private int yCardMany;
+    //[SerializeField] private float distance = 0.1f;
     [SerializeField] private GameObject prefab;
-    [SerializeField] private GameObject nullItem;
+    //[SerializeField] private GameObject nullItem;
+    [SerializeField] Transform Parent;
     List<IngredientTypeEnum> BuyThing = new List<IngredientTypeEnum>();
-    private Vector2 startVetor;
-    private Vector2 nowVecor;
+    //private Vector2 startVetor;
+    //private Vector2 nowVecor;
     private void Awake()
     {
         Initialize();
     }
     private void Initialize()
     {
-        transform.position = new Vector3(-(prefab.transform.localScale.x * 2 + distance * 2), prefab.transform.localScale.y * 2 + distance * 2, 0);
-        startVetor = transform.position;
-        startVetor = new Vector2(startVetor.x + prefab.transform.localScale.x / 2, startVetor.y - prefab.transform.localScale.y / 2);
-        nowVecor = startVetor;
+        //transform.position = new Vector3(-(prefab.transform.localScale.x * 2 + distance * 2), prefab.transform.localScale.y * 2 + distance * 2, 0);
+        //startVetor = transform.position;
+        //startVetor = new Vector2(startVetor.x + prefab.transform.localScale.x / 2, startVetor.y - prefab.transform.localScale.y / 2);
+        //nowVecor = startVetor;
         InitializeItems();
     }
     private void InitializeItems()
@@ -40,28 +41,43 @@ public class ShopSetting : MonoBehaviour
     }
     private void Start()
     {
-        for (int x = 0; x < xCardMany; x++)
+        //for (int x = 0; x < xCardMany; x++)
+        //{
+        //    for (int y = 0; y < yCardMany; y++)
+        //    {
+        //        if (BuyThing.Count > 0)
+        //        {
+        //            int rand = Random.Range(0, BuyThing.Count);
+        //            GameObject temp = Instantiate(prefab, nowVecor, Quaternion.identity, transform);
+        //            if (temp.transform.GetChild(0).TryGetComponent(out Item item) && BuyThing.Count > 0)
+        //            {
+        //                item.MyIngredientType = BuyThing[rand];
+        //                item.Initialize();
+        //                BuyThing.RemoveAt(rand);
+        //            }
+        //        }
+        //        else
+        //        {
+        //            Instantiate(nullItem, nowVecor, Quaternion.identity, transform);
+        //        }
+        //        nowVecor = new Vector2(nowVecor.x + distance + prefab.transform.localScale.x, nowVecor.y);
+        //    }
+        //    nowVecor = new Vector2(startVetor.x, nowVecor.y - distance - prefab.transform.localScale.y);
+        //}
+        for(int i = 0; i < BuyThing.Count; i++)
         {
-            for (int y = 0; y < yCardMany; y++)
+            int rand = Random.Range(0, BuyThing.Count);
+            GameObject temp = Instantiate(prefab , Parent);
+            if (temp.transform.GetChild(0).TryGetComponent(out Item item) && BuyThing.Count > 0)
             {
-                if (BuyThing.Count > 0)
-                {
-                    int rand = Random.Range(0, BuyThing.Count);
-                    GameObject temp = Instantiate(prefab, nowVecor, Quaternion.identity, transform);
-                    if (temp.transform.GetChild(0).TryGetComponent(out Item item) && BuyThing.Count > 0)
-                    {
-                        item.MyIngredientType = BuyThing[rand];
-                        item.Initialize();
-                        BuyThing.RemoveAt(rand);
-                    }
-                }
-                else
-                {
-                    Instantiate(nullItem, nowVecor, Quaternion.identity, transform);
-                }
-                nowVecor = new Vector2(nowVecor.x + distance + prefab.transform.localScale.x, nowVecor.y);
+                item.MyIngredientType = BuyThing[rand];
+                item.Initialize();
+                BuyThing.RemoveAt(rand);
             }
-            nowVecor = new Vector2(startVetor.x, nowVecor.y - distance - prefab.transform.localScale.y);
         }
+    }
+    public void SetCard(int i)
+    {
+        
     }
 }
