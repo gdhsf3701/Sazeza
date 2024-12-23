df52e44d0ce04e3293df0dae1e1c33c69c0f3808
moon080411 <anstjdalsdlek@gmail.com>
2024-12-23 18:05:54 +0900

    fix:stage1

--- Assets/01.Script/moon/Stage1.cs
+++ Assets/01.Script/moon/Stage1.cs
@@ -31,11 +31,11 @@ public class Stage1 : MonoBehaviour
     {
         if (Ingredient.Contains(add))
         {
-            GameManager.Instance.IngredientScore.Add(add,500 + GameManager.Instance.IngredientSO[add].PlusScore());
+            GameManager.Instance.PlusScore(500 + GameManager.Instance.IngredientSO[add].PlusScore());
         }
         else
         {
-            GameManager.Instance.IngredientScore.Add(add, 500 - (GameManager.Instance.IngredientSO[add].PlusScore()/2));
+            GameManager.Instance.PlusScore(500 - (GameManager.Instance.IngredientSO[add].PlusScore()/2));
         }
         GameManager.Instance.IngredientDirtyRate.Add(add, 0);
         if (nowIngredientCount >= 3)
